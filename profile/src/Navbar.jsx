c2f99713b6086d0a1f12c0d0c68ba29efadccf99
Mainudheen <mainudheens.23aid@kongu.edu>
2025-07-20 16:47:20 +0530

    navbar

---
+++ profile/src/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav id='navbar'>
+
+
+            <h2 id='text'>Mainudheen</h2>
+            <div id='nav-button'>
+                <button id='nav-button'>HOME</button>
+                <button id='nav-button'>ABOUT ME</button>
+                <button id='nav-button'>PROJECTS</button>
+                <button id='nav-button'>CERTIFICATION</button>
+            </div>
+
+
+        </nav>
+    )
+}
+
+export default Navbar
